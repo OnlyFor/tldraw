18ba0b057e27b790f26efd10241db236ebc0ad25
alex <alex@dytry.ch>
2024-08-22 18:39:18 +0100

    alex/rework-snapshot-tests: yeah

--- packages/utils/src/lib/bind.ts
+++ packages/utils/src/lib/bind.ts
@@ -1,29 +1,64 @@
 // MIT License: https://github.com/NoHomey/bind-decorator/blob/master/License
 // Copyright (c) 2016 Ivo Stratev
-/** @public */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function bind<T extends Function>(
-	_target: object,
+
+/**
+ * `@bind` is a decorator that binds the method to the instance of the class (legacy stage-2
+ * typescript decorators).
+ *
+ * @public
+ */
+export function bind<T extends (...args: any[]) => any>(
+	target: object,
 	propertyKey: string,
 	descriptor: TypedPropertyDescriptor<T>
-): TypedPropertyDescriptor<T> | void {
-	if (!descriptor || typeof descriptor.value !== 'function') {
-		throw new TypeError(
-			`Only methods can be decorated with @bind. <${propertyKey}> is not a method!`
-		)
-	}
+): TypedPropertyDescriptor<T>
 
-	return {
-		configurable: true,
-		get(this: T): T {
-			const bound: T = descriptor.value!.bind(this)
-			// Credits to https://github.com/andreypopp/autobind-decorator for memoizing the result of bind against a symbol on the instance.
-			Object.defineProperty(this, propertyKey, {
-				value: bound,
-				configurable: true,
-				writable: true,
-			})
-			return bound
-		},
+/**
+ * `@bind` is a decorator that binds the method to the instance of the class (TC39 decorators).
+ *
+ * @public
+ */
+export function bind<This extends object, T extends (...args: any[]) => any>(
+	originalMethod: T,
+	context: ClassMethodDecoratorContext<This, T>
+): void
+
+/** @public */
+export function bind(
+	...args: // legacy stage-2 typescript decorators
+	| [_target: object, propertyKey: string, descriptor: PropertyDescriptor]
+		// TC39 decorators
+		| [originalMethod: (...args: any[]) => any, context: ClassMemberDecoratorContext]
+): PropertyDescriptor | void {
+	if (args.length === 2) {
+		const [_originalMethod, context] = args
+		const methodName = context.name
+		if (context.private) {
+			throw new Error(`'bind' cannot decorate private properties like ${methodName as string}.`)
+		}
+		context.addInitializer(function (this: any) {
+			this[methodName] = this[methodName].bind(this)
+		})
+	} else {
+		const [_target, propertyKey, descriptor] = args
+		if (!descriptor || typeof descriptor.value !== 'function') {
+			throw new TypeError(
+				`Only methods can be decorated with @bind. <${propertyKey}> is not a method!`
+			)
+		}
+
+		return {
+			configurable: true,
+			get(this: any): any {
+				const bound = descriptor.value!.bind(this)
+				// Credits to https://github.com/andreypopp/autobind-decorator for memoizing the result of bind against a symbol on the instance.
+				Object.defineProperty(this, propertyKey, {
+					value: bound,
+					configurable: true,
+					writable: true,
+				})
+				return bound
+			},
+		}
 	}
 }
